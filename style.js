0f4f8daf81993381062e6c1e4912728d287fa348
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2024-05-22 13:48:20 -0300

    terminon

--- style.js
+++ style.js
@@ -27,3 +27,17 @@ var swiper = new Swiper(".mySwiper", {
     menu.classList.remove("move");
     navbar.classList.remove("open-menu");
   };
+
+  // SrollReveal 
+
+  const animate = ScrollReveal({
+    origin: 'top',
+    distance:'60px',
+    duration: '2500',
+    delay: '400',
+  });
+
+  animate.reveal(".nav");
+  animate.reveal(".home-text", {origin: "left"});
+  animate.reveal(".home-img", {origin: "bottom"});
+  animate.reveal(".ser-box, .product-box, .team-box, .book-data", {interval: 100});
