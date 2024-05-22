2ff19bd81eaa9a648fff947909e6ac7c9be27a1f
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2024-05-22 11:30:17 -0300

    atualizado

--- style.js
+++ style.js
@@ -11,3 +11,19 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  // Menu abrir fechar
+  let menu = document.querySelector('.menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+  };
+
+  // Fechar menu ao rolar
+
+  window.onscroll = () =>{
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+  };
